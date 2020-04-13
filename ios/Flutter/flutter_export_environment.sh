0d2f44b07ec2907352607fbb1f8923cbfda8609f
Cláudio Santos <cljos@hotmail.com>
2020-04-13 18:35:59 -0300

    Commit Project Dicee Flutter

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/claudiosantos/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/claudiosantos/Documents/cursos/udemy/angela-yu/dicee-flutter"
+export "FLUTTER_TARGET=/Users/claudiosantos/Documents/cursos/udemy/angela-yu/dicee-flutter/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/claudiosantos/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
